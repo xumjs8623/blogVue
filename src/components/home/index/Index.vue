6152d4b82c4d96ae5019f518691dbcda99e86ace
MJ <xumj8955@126.com>
2018-05-19 13:30:37 +0800

    首页编码

--- src/components/home/index/Index.vue
+++ src/components/home/index/Index.vue
@@ -1,61 +1,93 @@
 <template lang="html">
-  <el-row>
-    <!-- 头部 -->
-    <el-col :span="24" class="header">
-      <div class="header-content">
-        <el-row>
-          <el-col :span="8">
-            <div class="header-content-logo">
-              <div class="line-left-right">
-                <div class="line"></div>
-              </div>
-              <span class="title">MJ`home</span>
-              <div class="line-right-left">
-                <div class="line"></div>
-              </div>
-            </div>
-          </el-col>
-          <el-col :span="16" class="">
-            <ul class="header-menus">
-              <li>
-                <i class="iconfont icon-ai-home"></i>
-                <span>首页</span>
-              </li>
-              <li>
-                <i class="iconfont icon-fenlei"></i>
-                <span>分类</span>
-              </li>
-              <li>
-                <i class="iconfont icon-biaoqian"></i>
-                <span>标签</span>
-              </li>
-              <li>
-                <i class="iconfont icon-shangwutubiao-"></i>
-                <span>归档</span>
-              </li>
-              <li>
-                <i class="iconfont icon-guanyuwomen"></i>
-                <span>关于</span>
-              </li>
-              <li>
-                <i class="iconfont icon-qita"></i>
-                <span>其他</span>
-              </li>
-            </ul>
-          </el-col>
-        </el-row>
-      </div>
-    </el-col>
-    <el-col :span="24">
-      <div class="index-content">
-        <router-view></router-view>
-      </div>
-    </el-col>
-  </el-row>
+  <div class="index-class">
+    <div class="index-home" :style="homeStyle">
+      <el-row>
+        <!-- 头部 -->
+        <el-col :span="24" class="header">
+          <div class="header-content">
+            <el-row>
+              <el-col :span="8">
+                <div class="header-content-logo">
+                  <div class="line-left-right">
+                    <div class="line"></div>
+                  </div>
+                  <span class="title">MJ`home</span>
+                  <div class="line-right-left">
+                    <div class="line"></div>
+                  </div>
+                </div>
+              </el-col>
+              <el-col :span="16" class="">
+                <ul class="header-menus">
+                  <li>
+                    <i class="iconfont icon-ai-home"></i>
+                    <span>首页</span>
+                  </li>
+                  <li>
+                    <i class="iconfont icon-fenlei"></i>
+                    <span>分类</span>
+                  </li>
+                  <li>
+                    <i class="iconfont icon-biaoqian"></i>
+                    <span>标签</span>
+                  </li>
+                  <li>
+                    <i class="iconfont icon-shangwutubiao-"></i>
+                    <span>归档</span>
+                  </li>
+                  <li>
+                    <i class="iconfont icon-guanyuwomen"></i>
+                    <span>关于</span>
+                  </li>
+                  <li>
+                    <i class="iconfont icon-qita"></i>
+                    <span>其他</span>
+                  </li>
+                </ul>
+              </el-col>
+            </el-row>
+          </div>
+        </el-col>
+        <!-- 内容页 -->
+        <el-col :span="24">
+          <div class="index-content">
+            <router-view></router-view>
+          </div>
+        </el-col>
+        <!-- 侧边栏按钮 -->
+        <div class="sider-icon">
+          <div class="sider-info" @click="showUserInfo">
+            <i class="iconfont icon-fenlei1"></i>
+          </div>
+          <div class="sider-top">
+            <i class="iconfont icon-fanhuidingbu"></i>
+          </div>
+        </div>
+      </el-row>
+    </div>
+    <div class="index-info" :style="menusStyle">
+      <indexInfo></indexInfo>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  components: {
+    indexInfo: () => import('../userInfo/IndexInfo')
+  },
+  data () {
+    return {
+      homeStyle: {},
+      menusStyle: {}
+    }
+  },
+  methods: {
+    showUserInfo () {
+      this.homeStyle = {'paddingRight': '320px'}
+      this.menusStyle = {'width': '320px'}
+    }
+  }
 }
 </script>
 
---
+++ src/components/home/userInfo/IndexInfo.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+  <div class="index-info">
+    <el-row>
+      <!-- 头像 -->
+      <el-col :span="24">
+        <img src="" alt="">
+      </el-col>
+      <!-- 名称 -->
+      <el-col :span="24">
+        <span>MJ</span>
+      </el-col>
+      <!-- 统计 -->
+      <el-col :span="24">
+        <div class="article-count">
+        </div>
+        <div class="catergory-count">
+        </div>
+        <div class="tag-count">
+        </div>
+      </el-col>
+      <!-- 个人连接 -->
+      <el-col :span="24">
+        <el-row>
+          <!-- github -->
+          <el-col :span="8"></el-col>
+          <!-- 微博 -->
+          <el-col :span="8"></el-col>
+          <!-- 个人主页 -->
+          <el-col :span="8"></el-col>
+        </el-row>
+      </el-col>
+      <!-- 友情链接 -->
+      <el-col :span="24">
+        <el-col :span="8"></el-col>
+      </el-col>
+    </el-row>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.index-info {
+  background-color: #262a30;
+  width: 320px;
+}
+</style>
